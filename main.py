728ba5ee0936836860e0fcf3db1fb28da6cd305b
techwhiz <techwhiz.semantha@gmail.com>
2025-02-12 07:54:10 -0500

    feat: multisig generation

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 
 import os
 
+from utils import generate_multisig_account
+
 load_dotenv()
 
 TEAM_WALLET_SEED_PHRASE = os.getenv("TEAM_WALLET_SEED_PHRASE")
 NUMBER_OF_TEAM_WALLETS = os.getenv("NUMBER_OF_TEAM_WALLETS")
+MULTISIG_THRESHOLD = os.getenv("MULTISIG_THRESHOLD")
 sequence = max(int(NUMBER_OF_TEAM_WALLETS) - 1, 0)
 
 
@@ -20,6 +23,21 @@ def get_team_wallets(seed_phrase: str, sequence: int) -> list:
 
 
 if __name__ == "__main__":
+    # todo: generate team wallets
     team_wallets = get_team_wallets(TEAM_WALLET_SEED_PHRASE, sequence)
-    for wallet in team_wallets:
-        print(wallet.ss58_address)
+    print("team wallets:")
+    for account in team_wallets:
+        print(f"- {account.ss58_address}")
+
+    # todo: create multisig account
+    multisig_account = generate_multisig_account(
+        [wallet.ss58_address for wallet in team_wallets],
+        threshold=int(MULTISIG_THRESHOLD),
+    )
+    print(
+        f"multisig account ({MULTISIG_THRESHOLD}/{NUMBER_OF_TEAM_WALLETS}): ",
+        multisig_account.ss58_address,
+    )
+
+    # todo: trigger multisig extrinsic (from parent account)
+    # todo: approve multisig extrinsic (from team wallets)
--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
-from substrateinterface import Keypair
+from substrateinterface import Keypair, SubstrateInterface
 from dotenv import load_dotenv
 
 import os
 
 load_dotenv()
+
+
+chain_endpoint = os.getenv("CHAIN_ENDPOINT") or "wss://rpc.polkadot.io"
+print(f"chain endpoint: {chain_endpoint}")
+substrate = SubstrateInterface(url=os.getenv("CHAIN_ENDPOINT"))
 
 
 def get_keypair(seed_phrase: str) -> Keypair:
@@ -22,13 +27,13 @@ def generate_sequence_accounts(seed_phrase: str, sequence: int) -> list:
     return [generate_derived_keypair(seed_phrase, f"//{i}") for i in range(sequence)]
 
 
+def generate_multisig_account(signatories: list, threshold: int) -> Keypair:
+    return substrate.generate_multisig_account(signatories, threshold)
+
+
 if __name__ == "__main__":
     seed_phrase = os.getenv("TEAM_WALLET_SEED_PHRASE")
-    sequence = 9
-
-    keypair = get_keypair(seed_phrase)
-    print("parent: ", keypair.ss58_address)
-
+    sequence = int(os.getenv("NUMBER_OF_TEAM_WALLETS"))
     sequence_accounts = generate_sequence_accounts(seed_phrase, sequence)
     for account in sequence_accounts:
         print(account.ss58_address)
